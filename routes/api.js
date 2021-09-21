a4b774f432a034e3d30bea8530a480b2310728ec
_sekhaa <gansrey97@gmail.com>
2021-09-21 20:29:13 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["tegardev"];
+const listkey = ["APIKEY"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
